28655419288cedf89a95a0dca6e854adc9fdde5e
jefranca <jean13franca@gmail.com>
2021-12-18 09:12:43 -0300

    :tada: Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import { Outlet } from "react-router";
-export default function App(){
-
-    return(
-        <>
-            <Outlet/>
-        </>
-    )
+import styled from "styled-components";
+export default function App() {
+  return (
+    <BodyApp>
+      <Outlet />
+    </BodyApp>
+  );
 }
+
+const BodyApp = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  max-height: 100%;
+  background-color: #B0C4DE;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+  box-sizing: border-box;
+  padding: 10px;
+`;
